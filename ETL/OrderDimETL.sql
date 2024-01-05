3c226cb8772eac24738a0dfc4b5e79b81ef0e977
Ahmed Nasr <64994040+AHMEDNASR281@users.noreply.github.com>
2024-01-06 01:11:05 +0200

    new1

--- ETL/OrderDimETL.sql
+++ ETL/OrderDimETL.sql
@@ -1,25 +1,26 @@
 INSERT INTO [gravity_books_DWH].dbo.Dim_Order (
-    OrderID_SK, -- Surrogate key, typically auto-generated
     OrderID_BK,
     ShippingMethodID_BK,
     MethodName,
     Cost,
     HistoryID_BK,
     StatusID_BK,
-    StatusValue,
-    StartDate
+    StatusValue
 )
 SELECT 
-    NULL, -- Assuming the DWH auto-generates the surrogate key
     o.order_id,
     o.shipping_method_id,
     sm.method_name,
     sm.cost,
-    HistoryID_BK,
-    StatusID_BK,
-    StatusValue,
-    StartDate
+    oh.history_id,
+    OD.status_id,
+    OD.status_value
 FROM 
-[gravity_books].dbo.cust_order AS o
-JOIN [gravity_books].dbo.shipping_method AS sm ON o.shipping_method_id = sm.method_id
-JOIN [gravity_books].dbo.order_history os on sm.method_id = os.
+    [gravity_books].dbo.cust_order AS o
+INNER JOIN 
+    [gravity_books].dbo.shipping_method AS sm ON o.shipping_method_id = sm.method_id
+INNER JOIN 
+    [gravity_books].dbo.order_history AS oh ON o.order_id = oh.order_id
+INNER JOIN 
+    [gravity_books].dbo.order_status AS OD ON oh.status_id = OD.status_id;
+
